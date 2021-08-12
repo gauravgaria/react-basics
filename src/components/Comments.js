0625205fb8a930f46ee0362f20f19e062c164dbb
Gaurav <gaurav.garia93@gmail.com>
2021-08-12 15:46:39 +0530

    more props added

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import Image from '../logo.svg'
-import CommentDetails from './CommentDetails';
+import React from "react";
+import Image from "../logo.svg";
+import CommentDetails from "./CommentDetails";
 
+/*  props-> (propName = "value")
+ */
 
-/*  props-> (propName = "value") 
-*/ 
-
-const Comment = () =>{
-    return(
-        <div className="ui container comments">
-            <CommentDetails author="Gaurav" text="Nice Blog" img= {Image}/> 
-            <CommentDetails author="Nimesh" text="Your blog was quite helpful" img= {Image}/>
-            <CommentDetails author="Shilpa" text="Amazing blog" img= {Image}/>
-            <CommentDetails author="Deepesh" text="nice blog dude" img= {Image}/>
-        </div>
-    );
+const Comment = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetails author="Gaurav" text="Nice Blog" img={Image} />
+      <CommentDetails
+        author="Nimesh"
+        text="Your blog was quite helpful"
+        img={Image}
+      />
+      <CommentDetails author="Shilpa" text="Amazing blog" img={Image} />
+      <CommentDetails author="Deepesh" text="nice blog dude" img={Image} />
+    </div>
+  );
 };
 
-export default Comment
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import JsxBasics from './components/JsxBasics'
-import Comment from './components/Comments'
+import JsxBasics from "./components/JsxBasics";
+import Comment from "./components/Comments";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +13,7 @@ ReactDOM.render(
     <JsxBasics />
     <Comment />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
