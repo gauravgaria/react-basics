ce1ad0c133a8b7b30c6b2fd2d13661415f667e41
Gaurav <gaurav.garia93@gmail.com>
2021-08-12 16:44:21 +0530

    ui cards added

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,21 +1,41 @@
 import React from "react";
-import Image from "../logo.svg";
 import CommentDetails from "./CommentDetails";
-
+import ApprovalCard from "./ApprovalCard";
+import faker from "faker";
 /*  props-> (propName = "value")
  */
 
 const Comment = () => {
   return (
     <div className="ui container comments">
-      <CommentDetails author="Gaurav" text="Nice Blog" img={Image} />
-      <CommentDetails
-        author="Nimesh"
-        text="Your blog was quite helpful"
-        img={Image}
-      />
-      <CommentDetails author="Shilpa" text="Amazing blog" img={Image} />
-      <CommentDetails author="Deepesh" text="nice blog dude" img={Image} />
+      <ApprovalCard>
+        <CommentDetails
+          author="Gaurav"
+          text="Nice Blog"
+          img={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Nimesh"
+          text="Your blog was quite helpful"
+          img={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Shilpa"
+          text="Amazing blog"
+          img={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Deepesh"
+          text="nice blog dude"
+          img={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
