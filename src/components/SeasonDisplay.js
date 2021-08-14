b7e92ef85b24a833ce6ad7e3a8fe2aabf0e12491
Gaurav <gaurav.garia93@gmail.com>
2021-08-14 11:11:45 +0530

    some css es15 syntax

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import "../css/SeasonDisplay.css";
 import React from "react";
 
 const getSeason = (lat, month) => {
@@ -13,7 +14,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth);
-  return <div>Season Display : {season}</div>;
+  return (
+    <div>
+      <p className={`text-left ${season}`}>Season Display</p>{" "}
+      {/*${variable} direct assignment -> here season returns string winter/summer */}
+      <h2>{season}</h2>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
